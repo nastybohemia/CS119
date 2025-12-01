c9a2ccd3f5fcda55522d295970edb4746185d9f6
Sofia Popova <57526919+nastybohemia@users.noreply.github.com>
2025-11-30 20:10:36 -0800

    Add files via upload
    upload from Mac

--- CH8/DigitsStringSum/DigitsStringSum-1.py
+++ CH8/DigitsStringSum/DigitsStringSum-1.py
@@ -1,1 +1,32 @@
+# A Sum of Digits in a String
+# Author: Sofia Popova
+# LACC CS119
+# Task: A program that displayes the sum of a series of single-digit numbers with nothing separating them entered by a user.
 
+# get input a number string
+def get_input():
+    user_input = input("Enter a series of single-digit numbers with no spaces: ")
+    return user_input
+
+# calculations
+def calc_sum(numbers_str):
+    total = 0
+    for ch in numbers_str:
+        if ch.isdigit():
+            total += int(ch)
+    return total
+
+# display a formatted message for output 
+def display(numbers_str, result):
+    """Display a formatted message with the results."""
+    print("\n----- RESULT -----")
+    print(f"You entered: {numbers_str}")
+    print(f"The sum of the digits is: {result}")
+    print("-------------------\n")
+
+# Call main
+def main():
+    numbers = get_input()
+    result = calc_sum(numbers)
+    display(numbers, result)
+main()
